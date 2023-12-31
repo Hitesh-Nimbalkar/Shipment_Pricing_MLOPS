ecc22a94e863411f6b239ce90d136462cd0dbcc3
Hitesh-Nimbalkar <109200332+Hitesh-Nimbalkar@users.noreply.github.com>
2023-12-31 21:13:53 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+HYPERLINK_EDITABLE = "-e ."
+
+def get_requirements() -> List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
+        requirement_list = [line.strip() for line in requirements_file if line.strip()]
+
+    if HYPERLINK_EDITABLE in requirement_list:
+        requirement_list.remove(HYPERLINK_EDITABLE)
+
+    return requirement_list
+
+setup(
+    name="Machine_Failure",
+    version="0.0.1",
+    author="Hitesh Nimbalkar",
+    author_email="nimbalkarhitesh@gmail.com",
+    description="ML Regression ",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11"
+    ],
+    keywords="Regression",
+
+)
